4ef2704775000cb48ed632e131d9c95a1c9e8f2f
Gonçalo Frutuoso <goncasantacruz@gmail.com>
2026-09-11 00:47:22 +0100

    feat: replace portfolio with minimal static site

--- src/components/ui/AnimatedBackground.tsx
+++
@@ -1,9 +0,0 @@
-const AnimatedBackground = () => {
-  return (
-    <>
-      <div className="absolute inset-0 bg-gradient-to-b from-slate-50 to-white"></div>
-    </>
-  );
-};
-
-export default AnimatedBackground;
